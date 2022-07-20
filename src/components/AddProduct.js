f36d875c7d26dedcd598d2db1f9416721222d29f
Anubhav Guha <anubhav.guha@accenture.com>
2022-07-20 09:53:10 +0530

    add POST call method

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,20 +1,50 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Button, Container, Form, FormGroup, Input } from "reactstrap";
+import axios from "axios";
+import base_url from "../api/backend";
 
 const AddProduct = () => {
+    const [product, setProduct]=useState([]);
+    const handleForm=(e)=>{
+        console.log(product);
+        sendDataToServer(product);
+        e.preventDefault();
+
+    };
+    const sendDataToServer = (data) => {
+        axios.post(`${base_url}/addProduct`).then(
+            (response)=>{
+                console.log(response);
+                toast.success("API call successful !!");
+                setProduct(response.data);
+            },
+            (error)=>{
+                console.log(error);
+                toast.error("API call failed !");
+            }
+        );
+    };
+    useEffect(()=>{
+        document.title="Add Product | Page | React-Spring Application";
+        toast("Toast message from first hook !");
+    }, []);
     return(
         <Fragment>
-            <Form>
+            <Form onSubmit={handleForm}>
                 <FormGroup>
                     <label>Product ID</label>
-                    <Input type="text" placeholder="Enter Unique Integer" name="pid" id="pid"/>
+                    <Input type="text" placeholder="Enter Unique Integer" name="pid" id="pid" onChange={(e)=>{ 
+                       setProduct({...product, id: e.target.value}); 
+                    }}/>
                 </FormGroup>
                 <FormGroup>
                 <label>Product Name</label>
-                <Input type="text" placeholder="Enter name of the new Product" name="pname" id="pname"/>
+                <Input type="text" placeholder="Enter name of the new Product" name="pname" id="pname" onChange={(e)=>{ 
+                       setProduct({...product, name: e.target.value}); 
+                    }}/>
                 </FormGroup>
                 <Container className="text-center">
-                    <Button color="success">ADD Product</Button>
+                    <Button type="submit" color="success">ADD Product</Button>
                     <Button>CLR</Button>
                 </Container>
             </Form>
